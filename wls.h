49fd000db24f1e535ba13eb3e0b397cdb7b07007
Alexander W Blocker <ablocker@fas.harvard.edu>
2013-06-08 11:11:17 -0700

    Add calcFitted and calcResid definitions.

--- wls.h
+++ wls.h
@@ -2,3 +2,13 @@
 int wls(double* X, int n, int p, double* y, double* w,
         double* XTX, double *sqw, double* sqwX, double* sqwy, double* coef);
 
+int calcFitted(double* X, int n, int p,
+    double* y,
+    double* coef,
+    double* fitted);
+
+int calcResid(double* X, int n, int p,
+    double* y,
+    double* coef,
+    double* resid);
+
